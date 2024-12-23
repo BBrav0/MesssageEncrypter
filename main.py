e70706f31b996aaee639ebf7d68ba59deb6ab6f5
Ben Bravo <113952108+BBrav0@users.noreply.github.com>
2024-12-23 22:36:55 +0000

    working for some code words

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-//start
+#start
+def main():
+    inp = str(input("Input 'd' to decode or 'e' to encode\n"))
+    if (inp=="d"):
+        decode()
+    elif (inp=="e"):
+        encode()
+    else:
+        print("Invalid input.")
+        main()
+
+
+def decode():
+    mes = str(input("Enter your encrypted message\n"))
+    cod = str(input("Enter your code word\n"))
+    i = 0
+    nMes = ""
+    for char in mes:
+        if (i>=len(cod)):
+            i = 0
+        val = ord(char)-ord(cod[i])
+        nMes+=chr(val)
+        i+=1
+    print("Your decoded message:\n"+nMes)
+    
+
+def encode():
+    mes = str(input("Enter your message to encrypt\n"))
+    cod = str(input("Enter your code word\n"))
+    i = 0
+    nMes = ""
+    for char in mes:
+        if (i>=len(cod)):
+            i = 0
+        val = ord(char)+ord(cod[i])
+        nMes+=chr(val)
+        i+=1
+    print("Your encoded message:\n"+nMes)
+    print("Your codeword:\n"+cod)
+
+main()
