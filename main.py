6f8bb9a8ab086867ad99c488febb71ed475223c4
Ben Bravo <113952108+BBrav0@users.noreply.github.com>
2024-12-24 03:53:53 +0000

    finished

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-#start
 def main():
     inp = str(input("Input 'd' to decode or 'e' to encode or 'q' to quit\n"))
-    if (inp=="d"):
+    if inp == "d":
         decode()
-    elif (inp=="e"):
+    elif inp == "e":
         encode()
-    elif (inp=='q'):
-        quit
+    elif inp == 'q':
+        quit()
     else:
         print("Invalid input.")
         main()
@@ -17,13 +16,17 @@ def decode():
     cod = str(input("Enter your code word\n"))
     i = 0
     nMes = ""
+    printable_start = 32
+    printable_end = 126
+    printable_range = printable_end - printable_start + 1
+
     for char in mes:
-        if (i>=len(cod)):
+        if i >= len(cod):
             i = 0
-        val = ord(char)-ord(cod[i])
-        nMes+=chr(val)
-        i+=1
-    print("Your decoded message:\n"+nMes)
+        val = (ord(char) - ord(cod[i]) - printable_start) % printable_range + printable_start
+        nMes += chr(val)
+        i += 1
+    print("Your decoded message:\n" + nMes)
     main()
     
 
@@ -32,14 +35,19 @@ def encode():
     cod = str(input("Enter your code word\n"))
     i = 0
     nMes = ""
+    printable_start = 32
+    printable_end = 126
+    printable_range = printable_end - printable_start + 1
+
     for char in mes:
-        if (i>=len(cod)):
+        if i >= len(cod):
             i = 0
-        val = ord(char)+ord(cod[i])
-        nMes+=chr(val)
-        i+=1
-    print("Your encoded message:\n\""+nMes+"\"")
-    print("Your codeword:\n"+cod)
+        val = (ord(char) + ord(cod[i]) - printable_start) % printable_range + printable_start
+        nMes += chr(val)
+        i += 1
+    print("Your encoded message:\n\"" + nMes + "\"")
+    print("Your codeword:\n" + cod)
     main()
+
 
 main()
